98dfaeea94ca68acf2edd3f55fab24372b659d22
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-08 12:44:24 +0200

    Add files via upload

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,10 +1,3 @@
-
-       <script>
-       #!/usr/bin/node
+#!/usr/bin/node
         const myVar = "JavaScript is amazing"
         console.log(myVar);
-       </script>
-C:\Program Files\nodejs\node.exe .\0-javascript_is_amazing.js
-JavaScript is amazing
-
-
