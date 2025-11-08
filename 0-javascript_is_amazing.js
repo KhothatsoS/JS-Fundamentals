cce194e6b777902d6e36dc6633acd981e079bd01
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-08 12:40:06 +0200

    Update 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -4,5 +4,7 @@
         const myVar = "JavaScript is amazing"
         console.log(myVar);
        </script>
+C:\Program Files\nodejs\node.exe .\0-javascript_is_amazing.js
+JavaScript is amazing
 
 
